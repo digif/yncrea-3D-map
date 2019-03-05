2ae6103f62ff6533a77fe641046b3772cf72f8d5
digif <digifi@laposte.net>
2019-03-05 14:03:04 +0100

    making the magic work (oppening a info box has been implemented)

---
+++ Assets/InfoBox.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoBox : MonoBehaviour
+{
+    public GameObject infoBox;
+    private void OnTriggerEnter(Collider other)
+    {
+        infoBox.SetActive(!infoBox.activeInHierarchy);
+    }
+}
